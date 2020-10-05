8416cf6aa7f4e8557a7fad09a5c319c1c910d2af
makanning <martin@kanning.info>
2020-10-05 14:26:53 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and have fun!
         </p>
         <a
           className="App-link"
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
       </header>
     </div>
   );
